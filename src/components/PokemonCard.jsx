11cfa880cdc5de23e88816e7d2762f0295858295
douglas-camargo <douglascamargo159@gmail.com>
2024-11-22 19:15:12 -0400

    quitando carpeta utils

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -7,9 +7,29 @@ import CardHeader from './common/CardHeader';
 import { capitalize, formatPokemonId } from '../../utils';
 import CardTitle from './common/CardTitle';
 import usePokemonCard from '@/hook/usePokemonCard';
-import { typeColors } from '../../utils/utils';
 
 const PokemonCard = ({ name, url }) => {
+
+  const typeColors = {
+    normal: 'bg-gray-400',
+    fire: 'bg-red-500',
+    water: 'bg-blue-500',
+    electric: 'bg-yellow-400',
+    grass: 'bg-green-500',
+    ice: 'bg-blue-300',
+    fighting: 'bg-red-700',
+    poison: 'bg-purple-500',
+    ground: 'bg-yellow-600',
+    flying: 'bg-indigo-400',
+    psychic: 'bg-pink-500',
+    bug: 'bg-lime-500',
+    rock: 'bg-yellow-800',
+    ghost: 'bg-purple-700',
+    dragon: 'bg-indigo-700',
+    dark: 'bg-gray-800',
+    steel: 'bg-gray-500',
+    fairy: 'bg-pink-400',
+  };
   
   const { pokemon, loading } = usePokemonCard(url);
 
